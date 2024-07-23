83db268b87d3e347b6d63cdbf36f746517534535
mackwick <mackenziewicker@gmail.com>
2024-07-23 17:06:30 -0400

    started work on mobile under construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-// import logo from "./../public/images/logo.png";
-
 import "./App.css";
 
 function App() {
-  return <>This is the App.</>;
+  return (
+    <>
+      <p className="under-construction">
+        Our site is undergoing some maintenance. In the meantime, you can find
+        us on Instagram at [handle/link] and Facebook at [name/link]. You can
+        also email us at [insert address/link].
+      </p>
+    </>
+  );
 }
 
 export default App;
