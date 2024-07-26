2d1c843e97dbfab63342a5b52d1607bafea2ef59
mackwick <mackenziewicker@gmail.com>
2024-07-26 16:10:44 -0400

    UC mobile design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import "./App.css";
+import "./index.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <p className="under-construction">
-        Our site is undergoing some maintenance. In the meantime, you can find
-        us on Instagram at [handle/link] and Facebook at [name/link]. You can
-        also email us at [insert address/link].
-      </p>
-    </>
+    <div className="container">
+      <div className="under-construction">
+        <h2>The Mouths of Babes site is undergoing some maintenance. </h2>
+        <p>
+          For now, you can find us on Instagram at{" "}
+          <a href="https://www.instagram.com/mobtheatre/" className="links">
+            @mobtheatre
+          </a>{" "}
+          and Facebook at{" "}
+          <a
+            href="https://www.facebook.com/MoBTheatreCompany"
+            className="links"
+          >
+            Mouths of Babes Theatre Company
+          </a>
+          .
+        </p>
+        <p>
+          You can also email us at{" "}
+          <a href="mailto:mouthsofbabestheatre@gmail.com" className="links">
+            mouthsofbabestheatre@gmail.com
+          </a>
+          .
+        </p>
+        <p>Take care and come back soon.</p>
+      </div>
+    </div>
   );
 }
 
