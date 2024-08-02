1748a644edc53c8917b29f4ecbca56a7422abafd
mackwick <mackenziewicker@gmail.com>
2024-08-02 15:56:25 -0400

    extremely rough landing layout

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,26 @@
 function Landing() {
   return (
     <div className="landing-container">
-      <h1>Landing</h1>
+      <div className="landing-about">
+        <div className="brief-bio">
+          brief MOB intro and link to about page and donation button
+        </div>
+        <div className="landing-image">INSERT LANDING IMAGE</div>
+      </div>
+      <div className="landing-teaser">
+        current project teaser and link with image/collage background
+      </div>
+      <div className="latest-news">
+        <div className="news-item">
+          three images with brief blurbs: projects, events or calls to action
+        </div>
+        <div className="news-item">
+          three images with brief blurbs: projects, events or calls to action
+        </div>
+        <div className="news-item">
+          three images with brief blurbs: projects, events or calls to action
+        </div>
+      </div>
     </div>
   );
 }
