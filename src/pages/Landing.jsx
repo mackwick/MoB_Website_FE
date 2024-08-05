016ef519eb0c73e32f0efe536ae5409d4960e14c
mackwick <mackenziewicker@gmail.com>
2024-08-05 14:29:56 -0400

    rough top landing div set

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,22 @@
+import image from "/images/placeholder.jpeg";
+
 function Landing() {
   return (
     <div className="landing-container">
       <div className="landing-about">
         <div className="brief-bio">
-          brief MOB intro and link to about page and donation button
+          Mouths of Babes Theatre is dedicated to creating engaging,
+          interesting, and smart theatre for actors and audiences in high school
+          and college. We use forms of devising theatre to explore and
+          investigate issues we care about: engagement, risk, and advocacy.
+          Learn more about our mission. [LINK TO ABOUT]
+          <button>[LINK TO DONATE]</button>
         </div>
-        <div className="landing-image">INSERT LANDING IMAGE</div>
+        <div className="landing-bio-image">
+          <img src={image} className="landing-top-image" />
+        </div>
       </div>
+
       <div className="landing-teaser">
         current project teaser and link with image/collage background
       </div>
